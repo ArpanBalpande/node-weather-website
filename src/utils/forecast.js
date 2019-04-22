1fdca71e875976f1fecfc097ad42b01356ccc121
Arpan Balpande <arpan.balpande22@gmail.com>
2019-04-22 15:01:50 +0530

    Added high and low temperature features

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find the location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. With Highest Temperature as: ' + body.daily.data[0].temperatureHigh + ' and the Lowest Temperature as: ' + body.daily.data[0].temperatureLow)
         }
     })
 }
